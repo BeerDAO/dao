30258b9cd16304a116e3156017b4ee50601e96aa
Quint Daenen <me@di-wu.be>
2021-11-21 17:53:57 +0100

    Send link request to IC.

--- auth/accounts/test.sh
+++ auth/accounts/test.sh
@@ -5,7 +5,7 @@ dfx start --background --clean > /dev/null 2>&1
 dfx deploy --no-wallet
 
 id="$(dfx identity get-principal)"
-e=$(dfx canister --no-wallet call accounts addDiscordAccount "(record { id=\"0123456789\"; userName=\"test\"; discriminator=\"0001\" })")
+e=$(dfx canister --no-wallet call accounts addDiscordAccount "(record { id=\"0123456789\"; username=\"test\"; discriminator=\"0001\" })")
 dfx canister --no-wallet call accounts linkPrincipal "${e}"
 
 dfx -q stop > /dev/null 2>&1
